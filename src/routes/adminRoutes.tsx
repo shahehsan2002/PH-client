e48785a3c35f0a5f2099e2e09889a5981aee57da
Shah Ehsan <shahehsan2002@gmail.com>
2024-08-18 18:51:10 +0600

    Refactor hard coded admin routes

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,68 +1,77 @@
-import { Children } from "react";
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import CreateFaculty from "../pages/admin/CreateFaculty";
 
-export const adminPaths2 = [
+type TRoute ={
+  path:string,
+  element:ReactNode;
+}
+
+export const adminPaths = [
     {
         name:'Dashboard',
-        path:'/admin/dashboard',
+        path:'dashboard',
         element:<AdminDashboard/>
     },{
         name:"User Management",
         Children:[
             {
                 name:'Create Admin',
-                path:'/admin/create-admin',
+                path:'create-admin',
                 element:<CreateAdmin/>
             },{
                 name:'Create Faculty',
-                path:'/admin/create-faculty',
+                path:'create-faculty',
                 element:<CreateFaculty/>
             },{
                 name:'Create Student',
-                path:'/admin/create-student',
+                path:'create-student',
                 element:<CreateStudent/>
             },
         ]
-    },{
-        name:"Course Management",
-        Children:[
-            {
-                name:'Offered Course',
-                path:'/admin/offered-course',
-                element:<CreateAdmin/>
-            },{
-                name:'Create Faculty',
-                path:'/admin/create-faculty',
-                element:<CreateFaculty/>
-            },{
-                name:'Create Student',
-                path:'/admin/create-student',
-                element:<CreateStudent/>
-            },
-        ]
-    }
+    },
 ];
 
+// Programmatic way to flatten the routes
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
 
-//--------------------------
-export const adminPaths =[
-{
-    path:'dashboard',
-    element:<AdminDashboard/>
-},
-{
-    path:'create-admin',
-    element:<CreateAdmin/>
-},
-{
-    path:'create-faculty',
-    element:<CreateFaculty/>
-},
-{
-    path:'create-student',
-    element:<CreateStudent/>
-},
-]
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+
+  return acc;
+}, []);
+
+//--------Hard coded way------------------
+// export const adminPaths =[
+// {
+//     path:'dashboard',
+//     element:<AdminDashboard/>
+// },
+// {
+//     path:'create-admin',
+//     element:<CreateAdmin/>
+// },
+// {
+//     path:'create-faculty',
+//     element:<CreateFaculty/>
+// },
+// {
+//     path:'create-student',
+//     element:<CreateStudent/>
+// },
+// ]
+
